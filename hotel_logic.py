1217cb085912c3c30775e044d06273b25b54f64a
alexyungkk <w0517150@nscc.ca>
2026-05-02 16:29:47 -0300

    Feature: Add file upload, duplicate room validation, and timestamps in feedback.

--- hotel_logic.py
+++ hotel_logic.py
@@ -1,16 +1,35 @@
 import pandas as pd
+import time 
 from openpyxl import load_workbook
+
 
 account_sid = "YOUR_SID_HERE"
 auth_token = "YOUR_TOKEN_HERE"
 twilio_number = "+19027018815"
 my_phone_number = "+17826410444"
 
-roomSchedule = pd.ExcelFile('pages/room_schedule.xlsx')
-df = roomSchedule.parse('roomNum')
-rl = roomSchedule.parse('roomList')
-wb = load_workbook('pages/room_schedule.xlsx')
-ws = wb['roomList']
+ct = time.time()
+currTime = time.ctime(ct)
+# roomSchedule = pd.ExcelFile('pages/room_schedule.xlsx')
+# df = roomSchedule.parse('roomNum')
+# rl = roomSchedule.parse('roomList')
+# wb = load_workbook('pages/room_schedule.xlsx')
+# ws = wb['roomList']
+
+def schedule_rn(file):
+    roomSchedule = pd.ExcelFile(file)
+    rn = roomSchedule.parse('roomNum')
+    return rn
+
+def schedule_rl(file):
+    roomSchedule = pd.ExcelFile(file)
+    rl = roomSchedule.parse('roomList')
+    return rl
+
+def schedule_ws(file):
+    wb = load_workbook(file)
+    ws = wb['roomList']
+    return ws
 
 def hello():
     return "Hello, this is Alex's summer project"
--- pages/VacNoti.py
+++ pages/VacNoti.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import hotel_logic as hotel_logic
+import hotel_logic
 import openpyxl as xl
 from twilio.rest import Client
 
@@ -14,32 +14,46 @@ st.title(hotel_logic.hello())
 st.title("🏨 Room Dispatcher")
 st.write("Welcome, Supervisor. Select a room to notify an attendant.")
 
-with st.form("notification"):
-    room_number = st.text_input("Enter Room Number:", placeholder="room number")
-    send = st.form_submit_button("Send")
-    if send :
-        try:
-            int_room_number = int(room_number)
-            if int_room_number in hotel_logic.df.values:
-                # st.success(f"Notification {room_number}!")
-                if int_room_number in hotel_logic.rl.values:
-                    for row in hotel_logic.ws.iter_rows():
-                        for cell in row:
-                            if cell.value == int_room_number:
-                                st.success(f"Notification sent to {hotel_logic.ws.cell(row=cell.row, column=1).value} \
-                                        {hotel_logic.ws.cell(row=cell.row, column=2).value} for Room {room_number}!")
-                else:
-                    st.error(f"The room {room_number} is not assigned yet.")
-                    # st.success(f"Notification sent to for Room {room_number}!")
-                    
-            else:
-                # error if input invalid room number
-                st.error("Please input a valid room number.")
+schedule = st.file_uploader("Please upload today schedule", type=".xlsx", accept_multiple_files=False, 
+                            max_upload_size=10)
 
-        # error if input other than integer
-        except ValueError:
-            st.error("Please input a valid room number.")
-        
+if schedule is not None:
+    # hotel_logic.schedule_rn(schedule)
+    with st.form("notification"):
+        room_number = st.text_input("Enter Room Number:", placeholder="room number")
+        send = st.form_submit_button("Send")
+        if send:
+            try:
+                int_room_number = int(room_number)
+                if int_room_number in hotel_logic.schedule_rn(schedule).values:
+                    # st.success(f"Notification {room_number}!")
+                    if int_room_number in hotel_logic.schedule_rl(schedule).values:
+                        room_count = 0
+                        for row in hotel_logic.schedule_ws(schedule).iter_rows():
+                            for cell in row:
+                                if cell.value == int_room_number:
+                                    room_count += 1
+                        if room_count == 1:
+                            for row in hotel_logic.schedule_ws(schedule).iter_rows():
+                                for cell in row:
+                                    if cell.value == int_room_number:                                      
+                                        st.success(f"{hotel_logic.currTime} Notification sent to {hotel_logic.schedule_ws(schedule).cell(row=cell.row, column=1).value} \
+                                    {hotel_logic.schedule_ws(schedule).cell(row=cell.row, column=2).value} for Room {room_number}!")
+                        else:
+                            st.error(f"{hotel_logic.currTime} The room {room_number} is assigned to more than one room attendant.")
+                    else:
+                        st.error(f"{hotel_logic.currTime} The room {room_number} is not assigned yet.")
+                        # st.success(f"Notification sent to for Room {room_number}!")
+                        
+                else:
+                    # error if input invalid room number
+                    st.error(f"{hotel_logic.currTime} Please input a valid room number.")
+
+            # error if input other than integer
+            except ValueError:
+                st.error(f"{hotel_logic.currTime} Please input a valid room number.")
+
+
 
 
 
