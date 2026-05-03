04e7adc081c6759ec3b9b36f54c3371a2f5f35c8
alexyungkk <w0517150@nscc.ca>
2026-05-03 17:53:28 -0300

    Subject: Enhance logging, fix timestamps, and add template download
    Body:
    
    Fixed bug where timestamps in feedback messages remained static.
    
    Updated project flowchart to reflect new validation logic.
    
    Implemented a custom logger to track supervisor actions.
    
    Added st.download_button for the room schedule Excel template.

--- hotel_logic.py
+++ hotel_logic.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import time 
+
 from openpyxl import load_workbook
 
 
@@ -8,29 +8,41 @@ auth_token = "YOUR_TOKEN_HERE"
 twilio_number = "+19027018815"
 my_phone_number = "+17826410444"
 
-ct = time.time()
-currTime = time.ctime(ct)
-# roomSchedule = pd.ExcelFile('pages/room_schedule.xlsx')
+# roomSchedule = pd.ExcelFile('pages/room_schedule_template.xlsx')
 # df = roomSchedule.parse('roomNum')
 # rl = roomSchedule.parse('roomList')
 # wb = load_workbook('pages/room_schedule.xlsx')
 # ws = wb['roomList']
+# print(roomSchedule)
 
+# open excel file and read roomNum page
 def schedule_rn(file):
     roomSchedule = pd.ExcelFile(file)
     rn = roomSchedule.parse('roomNum')
     return rn
-
+# print(schedule_rn(roomSchedule))
+# open excel file and read roomList page
 def schedule_rl(file):
     roomSchedule = pd.ExcelFile(file)
     rl = roomSchedule.parse('roomList')
     return rl
 
+# open excel file and read roomList page by workbook function
 def schedule_ws(file):
     wb = load_workbook(file)
     ws = wb['roomList']
     return ws
 
+# function counting the number of input room in schedule
+def roomCount(roomNum, file):
+    room_count = 0
+    for row in schedule_ws(file).iter_rows():
+        for cell in row:
+            if cell.value == roomNum:
+                room_count += 1
+    return room_count
+
+# testing function
 def hello():
     return "Hello, this is Alex's summer project"
 
--- pages/VacNoti.py
+++ pages/VacNoti.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import hotel_logic
-import openpyxl as xl
+import datetime
 from twilio.rest import Client
+
 
 if not st.session_state.get("logged_in", False):
     st.error("Please login first!")
@@ -10,48 +11,88 @@ if not st.session_state.get("logged_in", False):
 
 # st.balloons()
 st.image("logo.png", width=400)
-st.title(hotel_logic.hello())
+st.write(hotel_logic.hello())
 st.title("🏨 Room Dispatcher")
-st.write("Welcome, Supervisor. Select a room to notify an attendant.")
+st.write("Welcome, Supervisor. Please use the template below")
+
+# Tell Python to look inside the pages folder
+with open("room_schedule_template.xlsx", "rb") as file:
+    btn = st.download_button(
+        label="Download the Room Schedule Template Excel File",
+        data=file,
+        file_name="room_schedule_template.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
 
 schedule = st.file_uploader("Please upload today schedule", type=".xlsx", accept_multiple_files=False, 
                             max_upload_size=10)
 
+currTime = datetime.datetime.now().strftime("%D, %H:%M:%S")
+
+# checking if no logger
+if "msg_logger" not in st.session_state:
+    # creating an empty logger list
+    st.session_state.msg_logger = []
+# function to append new messages to msg_logger
+def add_message(text, msg_type="info"):
+    st.session_state.msg_logger.insert(0, {"text": text, "type": msg_type})
+
+# checking if schedule has been uploaded
 if schedule is not None:
     # hotel_logic.schedule_rn(schedule)
+    # using form function
     with st.form("notification"):
         room_number = st.text_input("Enter Room Number:", placeholder="room number")
         send = st.form_submit_button("Send")
         if send:
             try:
                 int_room_number = int(room_number)
+                # Checking if inputed room number valid 
                 if int_room_number in hotel_logic.schedule_rn(schedule).values:
-                    # st.success(f"Notification {room_number}!")
+                    # checking if inputed room number assigned
                     if int_room_number in hotel_logic.schedule_rl(schedule).values:
-                        room_count = 0
-                        for row in hotel_logic.schedule_ws(schedule).iter_rows():
-                            for cell in row:
-                                if cell.value == int_room_number:
-                                    room_count += 1
-                        if room_count == 1:
+                        # checking if depluating assigned to room attentant
+                        if hotel_logic.roomCount(int_room_number, schedule) == 1:
+                            # finding the inputed room number in the schedule
                             for row in hotel_logic.schedule_ws(schedule).iter_rows():
                                 for cell in row:
-                                    if cell.value == int_room_number:                                      
-                                        st.success(f"{hotel_logic.currTime} Notification sent to {hotel_logic.schedule_ws(schedule).cell(row=cell.row, column=1).value} \
-                                    {hotel_logic.schedule_ws(schedule).cell(row=cell.row, column=2).value} for Room {room_number}!")
+                                    # text and adding success message if found the right room number and room attendant
+                                    if cell.value == int_room_number:
+                                        ra_name = hotel_logic.schedule_ws(schedule).cell(row=cell.row, column=1).value
+                                        ra_number = str(hotel_logic.schedule_ws(schedule).cell(row=cell.row, column=2).value)
+                                        # SMS senting part
+                                        # account_sid = hotel_logic.account_sid
+                                        # account_token = hotel_logic.auth_token
+
+                                        # client = Client(account_sid, account_token)
+
+                                        # message = client.messages.create(
+                                        #     from_=hotel_logic.twilio_number,
+                                        #     body= f"From supervisor. Hi, room {room_number} is vacant now, thank you",
+                                        #     to=ra_number
+                                        # )                                      
+                                        add_message(f"{currTime} Notification sent to {ra_name} {ra_number} for room {room_number}!", "success")                                       
                         else:
-                            st.error(f"{hotel_logic.currTime} The room {room_number} is assigned to more than one room attendant.")
+                            add_message(f"{currTime} Room {room_number} is assigned to more than one room attendants.", "error")
                     else:
-                        st.error(f"{hotel_logic.currTime} The room {room_number} is not assigned yet.")
-                        # st.success(f"Notification sent to for Room {room_number}!")
-                        
+                        add_message(f"{currTime} Room {room_number} is not assigned yet.", "error")                       
                 else:
                     # error if input invalid room number
-                    st.error(f"{hotel_logic.currTime} Please input a valid room number.")
+                    add_message(f"{currTime} Please input a valid room number.", "error")
 
             # error if input other than integer
             except ValueError:
-                st.error(f"{hotel_logic.currTime} Please input a valid room number.")
+                add_message(f"{currTime} Please input a valid room number.", "error")
+
+
+st.divider()
+
+st.subheader("Message History")
+for msg in st.session_state.msg_logger:
+    if msg["type"] == "success":
+        st.success(msg["text"])
+    elif msg["type"] == "error":
+        st.error(msg["text"])
 
 
 
